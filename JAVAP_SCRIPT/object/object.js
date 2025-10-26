f4f052760c0091caf30675c86f956f741274fa49
Mohan <mohanlokesh041@gamil.com>
2025-10-26 19:57:44 +0530

    updated

---
+++ JAVAP_SCRIPT/object/object.js
@@ -0,0 +1,39 @@
+let restaurantName = { name:"aasifa"};
+console.log(restaurantName);
+
+let restaurant_address = { address:"redhills bus stand"};
+console.log(restaurant_address);
+
+let restaurantLocation= {location1:"chennai"};
+console.log(restaurantLocation.location1);
+
+let menu_category = { menu:"non-veg"};
+console.log(menu_category);
+
+let DishName = { dish:"Chicken Briyani"};
+console.log(DishName);
+
+let dishType = { type:"fish curry"};
+console.log(dishType);
+
+let dish_size = { size:"full meals"};
+console.log(dish_size);
+
+let dishFlavour = {flavour1:"Two Spicy"};
+console.log(dishFlavour);
+
+let TotalBriyani = {total:1,total2:{briyani:1,briyani:2}};
+console.log(TotalBriyani.total2.briyani);
+
+let desert_name = [{dessert1:"Icecream",dessert2:{sweet1:"Laddu",sweet2:"GulabJamu"}}];
+console.log(desert_name[0].dessert1);
+
+let PaymentMethod = [{payment1:"Cash"},{payment2:{payment3:"Gpay",payment4:"Phonepe"}},];
+console.log(PaymentMethod[0].payment1);
+
+
+
+let payment_status = [{paymentmethod1:"paid",paymentmethod2:{paymentmethod3:"not paid",paymentmethod4:"paid"}}];
+console.log(payment_status[0].paymentmethod2);
+
+
